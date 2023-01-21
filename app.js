1ad1e4becc8fbc49f274aee1ece5e9f588d5c816
ramazan-cakir <rmzncakirdim@gmail.com>
2023-01-21 13:41:36 +0300

    dd

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 let randomNumber = Math.ceil(Math.random() * 100);
 const input = document.querySelector(".input");
 const buton = document.querySelector(".input-con button");
-let live = 6;
+const result = document.querySelector(".result");
+const imgCon = document.querySelector(".img-con");
+let live = 3;
 
-console.log(buton);
-
+// console.log((result.innerText = "Congragulations")); // Result un içinde kazanma yada kaybetme durumunda innerText ile yeni yazılar yazabiliriz. Ama önce const ve querySelector ile ulaşmamız lazım
+// console.log(imgCon);
 //Değişkenler
 // ceil() sayıyı yukarı yuvarlar
 
 // 1-100 arasında sayı tut
-console.log(randomNumber);
+// console.log(randomNumber);
 
 //addEventListener herhangi bir elementi takip eder, dinler. tıklandığında (click lendiğinde )
 
@@ -22,22 +24,26 @@ buton.addEventListener("click", () => {
 
   if (+input.value === randomNumber) {
     console.log("Bildiniz");
+    result.innerText = "Congragulations";
+    imgCon.style.backgroundImage = "url('./assets/Congragulations.gif')";
   }
-
   //tahmin edilen sayı küçükse
 
   if (+input.value < randomNumber) {
     console.log("Arttır");
+    result.innerText = "Please enter higher number ⬆️";
   }
-
   //Tahmin edilen sayı büyükse
 
   if (+input.value > randomNumber) {
     console.log("Azalt");
+    result.innerText = "Please enter lower number ⬇️";
   }
 
   //can bitince
   if (live == 0) {
     console.log("Kaybettiniz can bitti");
+    result.innerText = "Try Again!! ";
+    imgCon.style.backgroundImage = "url('./assets/try-again.gif')";
   }
 });
